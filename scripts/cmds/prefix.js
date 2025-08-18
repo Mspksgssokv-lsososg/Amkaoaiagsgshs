906cea33fcd5bd160e1a043a670bdb6cf893a9c6
4G-RAM <itssiddik487@gmail.com>
2025-08-18 08:52:53 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,7 +1,6 @@
 module.exports = {
   config: {
     name: "prefix",
-    aliases: ["Prefiz2", "px2"],
     version: "1.2",
     author: "SK-SIDDIK-KHAN",
     countDown: 5,
@@ -15,26 +14,32 @@ module.exports = {
     try {
       if (!event.body || event.body.toLowerCase() !== "prefix") return;
 
-      const userData = await usersData.get(event.senderID);
-      const username = userData?.name || "User";
       const botPrefix = global.GoatBot?.config?.prefix || "No prefix set";
-      const imageUrl = "https://drive.google.com/uc?export=download&id=12YtKMHbqz_Aj5GNiPkHjQ-Jia6icDbDn";
+
+      const serverTime = new Date().toLocaleTimeString("en-BD", {
+        timeZone: "Asia/Dhaka",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+      });
 
       return message.reply({
         body:
-          `┏━━━━━━━━━━━━━━━━━━☾︎\n` +
-          `├‣ 𝐏𝐑𝐄𝐅𝐈𝐗-𝐂𝐌𝐃\n` +
-          `├‣ ${username}\n` +
-          `├‣ 𝐁𝐎𝐓-𝐏𝐑𝐄𝐅𝐈𝐗:【 ${botPrefix} 】\n` +
-          `├‣ 𝐎𝐖𝐍𝐄𝐑: 𝐒𝐊-𝐒𝐈𝐃𝐃𝐈𝐊\n` +
-          `┗━━━━━━━━━━━━━━━━━━☾︎`,
-        mentions: [{ tag: username, id: event.senderID }],
-        attachment: await global.utils.getStreamFromURL(imageUrl),
+`♡  ∩_∩
+（„• ֊ •„)♡
+╭─∪∪────────────⟡
+│𝐏𝐑𝐄𝐅𝐈𝐗-𝐂𝐌𝐃-𝐈𝐍𝐅𝐎
+├───────────────⟡
+│🌍 𝐒𝐲𝐬𝐭𝐞𝐦 𝐏𝐫𝐞𝐟𝐢𝐱: ${botPrefix}
+├───────────────⟡
+│⏰ 𝐓𝐢𝐦𝐞: ${serverTime}
+╰───────────────⟡`,
+        attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/FKpvnK68/launcher-icon.jpg")
       });
 
     } catch (error) {
-      console.error("Error in prefix command:", error);
-      return message.reply("Error");
+      console.error("Prefix command error:", error);
     }
-  },
+  }
 };

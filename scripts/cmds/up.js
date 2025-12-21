2453a1b830b89b1be704298a5744517dfa7bddc2
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2025-12-21 14:09:08 +0600

    Update up.js

--- scripts/cmds/up.js
+++ scripts/cmds/up.js
@@ -4,7 +4,7 @@ const { formatDuration, intervalToDuration } = require('date-fns');
 
 const botStartTime = Date.now();
 
-const imageUrl = "https://graph.facebook.com/100000667148369/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+const imageUrl = "https://graph.facebook.com/100005002412625/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
 
 module.exports = {
   config: {
